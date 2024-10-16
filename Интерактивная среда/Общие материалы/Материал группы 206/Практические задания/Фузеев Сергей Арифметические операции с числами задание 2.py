736fac1e7b8ebd0daa5cded15ed19fb39f89dac3
BotAdmin-coderITSE <superbot367@outlook.com>
2024-10-16 11:08:03 +0300

    vault backup from user KGMT-bot: 2024-10-16 11:08:01

---
+++ Интерактивная среда/Общие материалы/Материал группы 206/Практические задания/Фузеев Сергей Арифметические операции с числами задание 2.py
@@ -0,0 +1,9 @@
+def calculate_interest(principal, rate, time):
+    return (principal * rate * time) / 100
+try:
+    P = float(input("Начальная сумма вклада: "))
+    R = float(input("Начисленные проценты: "))
+    T = float(input("Количество лет: "))
+    interest = calculate_interest(P, R, T)
+except ValueError:
+    print("Пожалуйста, введите корректные числовые значения.")
